6ee253225a40bdb01597d70e7f126e0353a109e6
yaroslavabramov <ya.abr97@gmail.com>
2017-05-22 08:18:08 +0300

    router+styled-components

--- src/Products/Show/BuyNow/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './style.css'
-
-export default ()=> {
-  return(
-        <button className="buy-now-button">buy now</button>
-  );
-};
---
+++ src/Sidebar/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Searchbar from './Searchbar';
+import Menu from './Menu';
+
+const Sidebar = styled.aside`
+  margin:0;
+  padding:0;
+  width: 414px;
+  min-height: 1171px;
+  background-color: #0e0e0e;
+  background-image: url(${require('./images/logo.png')});
+  background-repeat: no-repeat;
+  background-position: center 50px;
+`;
+
+
+export default ()=> {
+  return(
+    <Sidebar>
+      <Searchbar />
+      <Menu />
+    </Sidebar>
+  );
+};
