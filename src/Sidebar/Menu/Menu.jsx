6e6438fb5263185b5110e8b454a5e677cbb3b0c4
yaroslavabramov <ya.abr97@gmail.com>
2017-05-29 15:30:29 +0300

    fix names

--- src/Sidebar/Menu/Menu.jsx
+++ src/Sidebar/Menu/Menu.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import Arrow from '../images/group-2.svg';
+import arrow from '../images/group-2.svg';
 
 const Wrapper = styled.div`
   display: flex;
@@ -8,7 +8,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Title = styled.button`
+const Button = styled.button`
   font-size: 24px;
   text-transform: uppercase;
   margin-top: 25px;
@@ -24,7 +24,7 @@ const Title = styled.button`
     content: '';
     width: 14px;
     height: 9px;
-    background-image: url(${Arrow});
+    background-image: url(${arrow});
     background-repeat: no-repeat;
     transition: all 0.2s;
     ${props => props.isActive && `
@@ -50,9 +50,9 @@ export default class Menu extends Component {
   render() {
     return (
       <Wrapper>
-        <Title onClick={this.handleClick} isActive={this.state.isActive} >
+        <Button onClick={this.handleClick} isActive={this.state.isActive} >
           {this.props.title}
-        </Title>
+        </Button>
         {this.state.isActive && this.props.children}
       </Wrapper>
     );
